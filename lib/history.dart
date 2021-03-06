f111e93d834bf7db08fcda1fa01b023df1604a47
Bharath Nair <bharath.rajeevnair@student.adelaide.edu.au>
2021-03-07 02:42:57 +1030

    history

--- lib/history.dart
+++ lib/history.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:toast/toast.dart';
 
 class HistoryPage extends StatefulWidget {
   HistoryPage({Key key}) : super(key: key);
@@ -9,6 +10,7 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
+  List model = [{}, {}, {}];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,13 +29,30 @@ class _HistoryPageState extends State<HistoryPage> {
           children: <Widget>[
             Expanded(child: TopWidget()),
             // CenterWidget(),
-            SizedBox(
-              height: 300,
-            ),
-            Expanded(child: BottomWidget()),
+            // SizedBox(
+            //   height: 300,
+            // ),
+            // Expanded(child: BottomWidget()),
           ],
         ),
       ),
+      floatingActionButton: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          color: Color(0xFFFDEAD9),
+          width: 300,
+          height: 100,
+          child: Center(
+            child: Text(
+              "Authenticate",
+              style: GoogleFonts.montserrat(
+                  fontSize: 25, color: Color(0xFFF8A961)),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 
@@ -48,25 +67,110 @@ class _HistoryPageState extends State<HistoryPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Positioned(
-                top: 0.0,
+                //top: 0.0,
                 //right: 0.0,
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: new IconButton(
-                      icon: Icon(
-                        Icons.account_circle,
-                        size: 45,
-                        color: Color(0xFFF8A961),
-                      ),
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/home');
-                      }),
+                    icon: Icon(
+                      Icons.account_circle,
+                      size: 60,
+                      color: Color(0xFFF8A961),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                  ),
                 ),
               ),
-              // SizedBox(
-              //   width: 80,
-              // ),
             ],
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          new GestureDetector(
+            onTap: () {
+              Toast.show("Copied to clipboard!", context,
+                  duration: Toast.LENGTH_SHORT, gravity: Toast.BOTTOM);
+            },
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Your Unique ID: ",
+                  style: GoogleFonts.montserrat(
+                    fontSize: 25,
+                  ),
+                ),
+                SizedBox(
+                  width: 1,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    color: Color(0xFFF1F1F1),
+                    width: 500,
+                    height: 30,
+                    child: Center(
+                      child: Text(
+                        "VSBOWUV73RGFUWQ7FSD",
+                        style: GoogleFonts.montserrat(
+                            fontSize: 25, color: Colors.green),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  "Tap to copy",
+                  style: GoogleFonts.montserrat(color: Color(0xFFF8A961)),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          _myListView(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _myListView(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+        //padding: const EdgeInsets.all(5.0),
+        height: 400,
+        width: 500,
+        // decoration: BoxDecoration(
+        //   border: Border.all(
+        //     width: 4,
+        //     // color: EqualistColors.lightGreen,
+        //   ),
+        // ), //       <--- BoxDecoration here
+        child: ListView.builder(
+          itemCount: model.length,
+          itemBuilder: (context, index) {
+            return listItem(context, index);
+          },
+        ));
+  }
+
+  Widget listItem(BuildContext context, int index) {
+    return Card(
+      child: Row(
+        children: <Widget>[
+          Container(margin: EdgeInsets.all(10), child: Text("1")),
+          Container(
+            height: 20,
+            width: 1,
+            color: Colors.blue,
+          ),
+          Container(margin: EdgeInsets.all(10), child: Text("asdasd")),
+          SizedBox(
+            height: 50,
           ),
         ],
       ),
